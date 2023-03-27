717f30f20bc22ee74dad615664b49931f2d8a57f
shahrokh-t <tahmoores54@gmail.com>
2023-03-27 12:20:02 +1100

    handle error and exception

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,31 @@
-const registerUser = (req, res) => {
-    res.send("Register Route");
-};
+const asyncHandler = require("express-async-handler");
 
-const loginUser = (req, res) => {
+// @description    Register a new user
+// @route          /api/users
+// @access         public
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body;
+
+    // Validation
+    if(!name || !email || !password) {
+      res.status(400);
+      throw new Error("Please include all fields!");
+    }
+
+    res.send("Register Route");
+});
+
+
+// @description    Login a  user
+// @route          /api/users/login
+// @access         public
+const loginUser = asyncHandler(async (req, res) => {
     res.send("Login Route");
-};
+});
+
+
+
+
 
 module.exports = {
     registerUser,
